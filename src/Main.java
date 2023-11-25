9083a0ffbb15ad3d25a82cf8d656a573b06cbd69
naim <naimrzm17@gmail.com>
2023-11-25 16:19:06 +0800

    basic

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println(1+1);
+        System.out.println("Hello and welcome!");
+
+        for ( int i = 1; i<9; i++){
+            System.out.println("hii");
+        }
+
+
+
+
+
+        }
+    }
