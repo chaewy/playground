0756af6deb5b22d622aaec98a24317f6fcb5c383
naim <naimrzm17@gmail.com>
2023-11-28 18:51:33 +0800

    overloaded constructor

--- src/Car.java
+++ src/Car.java
@@ -4,12 +4,19 @@ public class Car {
     String origin;
     Integer power;
 
+    Car(String make){
+        this.make = make;
+
+    }
+    Car(String make, String origin){
+        this.make = make;
+        this.origin = origin;
+    }
     Car(String make, String origin, Integer power){
         this.make = make;
         this.origin = origin;
         this.power = power;
     }
-
     void desc(){
         System.out.println(this.make + " is from "+this.origin + " and equipped with "+ this.power + " HP engine");
     }
--- src/Main.java
+++ src/Main.java
@@ -2,25 +2,20 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Car car1 = new Car("Porsche","Germany",900);
-        Car car2 = new Car("Audi","Germany",800);
+        Car car1 = new Car("Porsche");
+        Car car2 = new Car("Audi","Germany");
 
         System.out.println(car1.make);
         System.out.println(car1.origin);
-        System.out.println(car1.power+" HP");
-        System.out.println();
+        System.out.println(car1.power);
         System.out.println(car2.make);
         System.out.println(car2.origin);
-        System.out.println(car2.power+" HP");
+        System.out.println(car2.power);
 
         System.out.println();
 
         car1.desc();
         car2.desc();
-
-
-
-
 
         }
 
