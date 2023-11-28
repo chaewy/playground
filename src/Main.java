071d0f1b9a082313881e3fe867ca3601f113a330
naim <naimrzm17@gmail.com>
2023-11-28 18:00:59 +0800

    overloaded method in main class

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,29 @@
 public class Main {
 
     public static void main(String[] args) {
-        
+
         System.out.println("Hello and welcome!");
         System.out.println();
 
 
-        int z = add(3,4);
-        System.out.println("result is "+z);
+        int result = add(3,4,10,3);
+        System.out.println(result);
+
 
         }
 
         static int add(int x,int y){
-            int z = x + y;
-            return z;
+            System.out.println("this is method 1");
+            return x+y;
+        }
+
+        static int add(int x, int y, int z){
+            System.out.println("This is method 2");
+            return x + y + z;
+        }
+
+        static int add(int a, int b, int c, int d){
+            System.out.println("This is method 3");
+            return a + b + c + d;
         }
     }
