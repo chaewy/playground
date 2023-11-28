3ce97fa6cf4d3676fe338bb622c5de271fef75c3
naim <naimrzm17@gmail.com>
2023-11-28 18:12:34 +0800

    object and call object method

---
+++ src/Car.java
@@ -0,0 +1,14 @@
+public class Car {
+
+    String make = "porsche";
+    String origin = "german";
+    Integer power = 900;
+
+    void gas(){
+        System.out.println("This car is moving");
+    }
+    void brake(){
+        System.out.println("This car is stopping");
+    }
+
+}
