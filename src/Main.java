6fe958949bcbd392a920df0a40b9775d34cbd33a
naim <naimrzm17@gmail.com>
2023-11-28 15:55:30 +0800

    method in main class

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,18 @@
 public class Main {
 
     public static void main(String[] args) {
-
-        System.out.println(1+1);
+        
         System.out.println("Hello and welcome!");
+        System.out.println();
 
-        for ( int i = 1; i<9; i++){
-            System.out.println("hii");
+
+        int z = add(3,4);
+        System.out.println("result is "+z);
+
         }
 
-        String car[] = {"camaro","mustang"};
-
-        int num[][] = {{1,2,3,4,5},{6,7,8,9,10}};
-
-        for (int i = 0;i< num.length; i++){
-            for (int j = 0; j< num[i].length; j++){
-                System.out.println(num[i][j]);
-            }
-        }
-
-
-
-
-
-
-
+        static int add(int x,int y){
+            int z = x + y;
+            return z;
         }
     }
