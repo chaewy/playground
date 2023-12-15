2853057b92538eb05ab20a301351cf0e6a581853
naim <naimrzm17@gmail.com>
2023-12-15 08:07:20 +0800

    polymorphism

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,24 @@
-public class Main {
+class Animal{
 
+    void sound(){
+        System.out.println("the animal is make a sound");
+    }
+}
+
+class Cat extends Animal{
+    @Override
+    void sound(){
+        System.out.println("the cat is making sound");
+    }
+}
+
+public class Main {
     public static void main(String[] args) {
 
-       Car car1 = new Car("panzer");
-       Car car2 = new Car("stuka");
-       Car car3 = new Car("junka");
-
-        //put car in garage array
-       Car[] garage = {car1,car2,car3};
-
-       for( Car i : garage){
-           System.out.println(i.make);
-       }
-
-
-       System.out.println();
-       System.out.println(garage[0].make);
-       System.out.println(garage[1].make);
-       System.out.println(garage[2].make);
+        Cat cat = new Cat();
+        Animal animal = new Animal();
+        animal.sound();
+        cat.sound();
 
     }
 }
